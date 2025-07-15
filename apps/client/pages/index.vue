4bf3cfe1bbb46c32d951a39aa9de7a36aafc70cc
wherewhale <syh980130@gmail.com>
2025-07-15 22:24:10 +0900

    [✨FEAT]: 기본 Nuxt 템플릿 설정 및 테스트 코드 추가

---
+++ apps/client/pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This is a simple Vue component for testing purposes
+
+import { ref } from "vue";
+
+const count = ref(0);
+const increment = () => {
+  count.value++;
+};
+</script>
+
+<template>
+  <div
+    class="max-w-sm mx-auto mt-10 flex flex-col items-center justify-center gap-4"
+  >
+    <div>웹 페이지 테스트</div>
+    <div class="text-2xl font-bold">카운트: {{ count }}</div>
+    <UButton
+      variant="solid"
+      class="px-8 py-3 text-lg cursor-pointer active:bg-blue-600 active:text-white w-full"
+      @click="increment"
+    >
+      <span class="w-full">카운트 증가</span>
+    </UButton>
+    <UIcon name="fluent-color:alert-20" class="text-4xl text-blue-500" />
+  </div>
+</template>
