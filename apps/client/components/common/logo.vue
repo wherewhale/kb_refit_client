422fc081e03224e873ef4cc77b10ebac194de4ea
wha1e <54974046+wherewhale@users.noreply.github.com>
2025-07-21 22:33:34 +0900

    Delete apps/client/components/common/logo.vue

--- apps/client/components/common/logo.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <NuxtLink to="/" class="w-20 relative block">
-    <NuxtImg
-      src="assets/images/logos/symbol-incase-logo.png"
-      alt="KB Refit Logo"
-    />
-  </NuxtLink>
-</template>
