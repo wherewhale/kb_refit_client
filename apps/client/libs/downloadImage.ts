f193ac04f492fd70cb024741b4bb83a21371fbcf
wha1e <syh980130@gmail.com>
2025-07-31 11:39:39 +0900

    [🐛FIX]: 주석 제거

--- apps/client/libs/downloadImage.ts
+++ apps/client/libs/downloadImage.ts
@@ -5,9 +5,5 @@ export async function downloadImage(filename: string): Promise<string> {
   // 확장자 추출
   const fileRef = storageRef(storage, `uploads/${filename}`);
 
-  // 파일 URL 반환
-  console.log("파일 URL:", fileRef.fullPath);
-  console.log("파일 다운로드 URL:", await getDownloadURL(fileRef));
-
   return await getDownloadURL(fileRef);
 }
