e3ce29e87544b05a627c1c61634a2b15a1f27891
wherewhale <syh980130@gmail.com>
2025-08-15 20:37:11 +0900

    [🐛FIX]: 푸시 알림 디자인 수정

--- apps/client/components/common/layout/CommonFooter.vue
+++ apps/client/components/common/layout/CommonFooter.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { removeTokens } from "@/utils/token";
+import { getToken } from "@firebase/messaging";
 
 const router = useRouter();
 
@@ -7,6 +8,41 @@ const onClickRemoveTokens = async () => {
   await removeTokens();
   router.push("/auth/login");
 };
+
+const messagingToken = ref("");
+
+onMounted(() => {
+  requestPermission();
+});
+
+function requestPermission() {
+  if (!window.Notification) return;
+
+  if (window.Notification.permission === "granted") {
+    setToken();
+  } else {
+    window.Notification.requestPermission((value) => {
+      if (value === "granted") {
+        setToken();
+      }
+    });
+  }
+}
+
+async function setToken() {
+  const { $messaging } = useNuxtApp();
+  const token = await getToken($messaging, {
+    vapidKey:
+      "BKOStP9Dtijt8pt9a3i_dOvDvXoeWvqViTBpRb-jAvwnlqbvGJToOKfGjrs4uCfbjLnofmwqMNxPAX6lLVe6yEM",
+  });
+  // TODO: 여기서 서버에 유저 정보 업데이트 API
+
+  messagingToken.value = token;
+}
+
+function copy() {
+  navigator.clipboard.writeText(messagingToken.value);
+}
 </script>
 
 <template>
@@ -20,6 +56,35 @@ const onClickRemoveTokens = async () => {
     >
       로그아웃
     </KBUITypography>
+    <KBUITypography
+      size="b14"
+      weight="medium"
+      color="gray-2"
+      class="cursor-pointer hover:text-gray-4 transition-colors mt-2"
+      @click="requestPermission"
+    >
+      푸시 알림 받기
+    </KBUITypography>
+    <div v-if="messagingToken">
+      <KBUITypography
+        size="b14"
+        weight="medium"
+        color="gray-2"
+        class="cursor-pointer hover:text-gray-4 transition-colors mt-2"
+      >
+        {{ messagingToken }}
+      </KBUITypography>
+      <KBUITypography
+        size="b14"
+        weight="medium"
+        color="gray-2"
+        class="cursor-pointer hover:text-gray-4 transition-colors mt-2"
+        @click="copy"
+      >
+        클립보드에 복사
+      </KBUITypography>
+    </div>
+
     <KBUITypography size="b14" weight="medium" color="gray-3">
       © 2025 깨비들. All rights reserved.
     </KBUITypography>
