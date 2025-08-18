ee352ae51b6a1312fbe12150aee76b8df04e4798
wha1e <syh980130@gmail.com>
2025-08-18 11:18:35 +0900

    [🐛FIX]: FCM 토큰 plain text로 변경

--- apps/client/services/fcm.ts
+++ apps/client/services/fcm.ts
@@ -1,5 +1,7 @@
 import { apiClient } from "./apiClient";
 
 export const postUserFcmToken = (token: string) => {
-  apiClient.post("/auth/fcm-token", { token });
+  apiClient.post("/auth/fcm-token", token, {
+    headers: { "Content-Type": "text/plain" }, // 서버가 단순 문자열을 기대한다면 필수
+  });
 };
